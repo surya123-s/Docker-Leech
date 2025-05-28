025b6b680b36dd15b564c570fa78f21e282bcd37
DOWNLOADER ZONE <downloaderzone1@gmail.com>
2025-05-28 08:12:31 +0000

    Added Mega Folder Download Support

--- bot/helper/mirror_leech_utils/status_utils/mega_dl_status.py
+++ bot/helper/mirror_leech_utils/status_utils/mega_dl_status.py
@@ -1,19 +1,29 @@
+from mega import MegaApi
+
 from ...ext_utils.status_utils import (
-    EngineStatus,
     MirrorStatus,
     get_readable_file_size,
-    get_readable_time,
+    get_readable_time
 )
 
 
 class MegaDownloadStatus:
-    def __init__(self, listener, obj, gid, status):
+    def __init__(
+            self,
+            listener,
+            obj,
+            gid,
+            status
+        ):
         self.listener = listener
         self._obj = obj
         self._size = self.listener.size
         self._gid = gid
         self._status = status
-        self.engine = EngineStatus().STATUS_MEGA
+        self.engine = f"Mega SDK v{self._eng_ver()}"
+
+    def _eng_ver(self):
+        return MegaApi("zee").getVersion()
 
     def name(self):
         return self.listener.name
@@ -21,21 +31,22 @@ class MegaDownloadStatus:
     def progress_raw(self):
         try:
             return round(self._obj.downloaded_bytes / self._size * 100, 2)
-        except ZeroDivisionError:
+        except:
             return 0.0
 
     def progress(self):
         return f"{self.progress_raw()}%"
 
     def status(self):
-        return MirrorStatus.STATUS_DOWNLOAD
+        return MirrorStatus.STATUS_DOWNLOADING
 
     def processed_bytes(self):
         return get_readable_file_size(self._obj.downloaded_bytes)
 
     def eta(self):
         try:
-            seconds = (self._size - self._obj.downloaded_bytes) / self._obj.speed
+            seconds = (self._size - self._obj.downloaded_bytes) / \
+                self._obj.speed
             return get_readable_time(seconds)
         except ZeroDivisionError:
             return "-"
